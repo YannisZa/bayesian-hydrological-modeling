95e3f1ce36d82904d39a3f7e4fb023b735e2f2f2
Yannis <yzachos@gmail.com>
2019-11-10 23:39:12 +0000

    added script to open nc files

---
+++ data-access/chess-evapotranspiration-data.py
@@ -0,0 +1,7 @@
+
+
+# List of all datasets
+# https://catalogue.ceh.ac.uk/datastore/eidchub/b745e7b1-626c-4ccc-ac27-56582e77b900/
+
+
+filename = '/Users/Yannis/code/fibe2-mini-project/Historic_daily_PET_McGuinness-Bordne_2015.nc'
