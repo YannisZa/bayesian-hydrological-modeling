abc52463793f78146908b602881b04cd88d9355d
Yannis <yzachos@gmail.com>
2019-11-10 23:39:26 +0000

    added comments

--- data-access/met-office-data-access.py
+++ data-access/met-office-data-access.py
@@ -3,15 +3,24 @@ import pandas as pd
 import requests
 import json
 
+# Define API key
 APIkey = 'ed84efb9-66bb-4d18-bd73-86a132112a60'
+# Stations url
 stations = 'val/wxfcs/all/json/sitelist'
 
+
+#hourly_data_by_location = 'val/wxobs/all/json/locationId'.format(locationId)
+
+# Define resource
 resource = stations
 
+# Define HTTP get url
 url = 'http://datapoint.metoffice.gov.uk/public/data/{resource}?key={APIkey}'.format(resource=resource,APIkey=APIkey)
 print(url)
+# Retrieve data
 response = requests.get(url)
 
+# If data is successfully retrieved
 if(response.ok):
 
     # Loading the response data into a dict variable
@@ -21,20 +30,15 @@ if(response.ok):
     print("\n")
     #print(json.dumps(data, indent=4, sort_keys=True))
 
-    #print(type(data))
+    # Convert data to dataframe
     df = pd.DataFrame.from_dict(data['Locations']['Location'])
 
+    # Save dataframe to file
     df.to_csv('met-office-station-locations.csv',index=False)
-
 
     # with open('met-office-station-locations.json', 'w') as f:
     #     json.dump(data, f)
 
 else:
-  # If response code is not ok (200), print the resulting http error code with description
+    # If response code is not ok (200), print the resulting http error code with description
     response.raise_for_status()
-
-
-#print(response.json())
-
-#hourly_data_by_location = 'val/wxobs/all/datatype/locationId'.format(locationId)
