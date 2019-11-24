a8f3ba42c85f63a5b7f91b53d7a11bfe91d4682d
Yannis <yzachos@gmail.com>
2019-11-24 23:44:26 +0000

    fixed bugs in LRM

--- models/physical/LinearReservoirModel.py
+++ models/physical/LinearReservoirModel.py
@@ -7,7 +7,7 @@ import os
 
 n_states = 1
 n_odeparams = 1
-n_ivs = 0
+n_ivs = 1
 
 class LinearReservoirModel(object):
 
@@ -26,6 +26,8 @@ class LinearReservoirModel(object):
 
     def _simulate(self, parameters, times, net_rainfall_data, sensitivities):
         k, q0 = [x for x in parameters]
+        # print('k',k)
+        # print('q0',q0)
 
         # Interpolate net_rainfall
         nr_int = interp1d(times, net_rainfall_data,fill_value="extrapolate",kind='slinear')
@@ -35,34 +37,32 @@ class LinearReservoirModel(object):
             return (nrint(time) - q) * (1./k)
 
         if sensitivities:
-            def jac(q,t,nrint):
+            def jac(x):
                 ret = np.zeros((self._n_states, self._n_states))
-                ret[0, 0] = (-1./(k**2)) * (nrint(t) - q)
+                ret[0, 0] = (-1./k)
                 return ret
 
-            def dfdp(y):
-                X, Y = y
+            def dfdp(x,t,nrint):
                 ret = np.zeros((self._n_states,
                                 self._n_odeparams + self._n_ivs))
-                ret[0, 0] = (-1./k)
+                ret[0, 0] = (-1./(k**2)) * (nrint(t) - x)
                 return ret
 
             def rhs(q_and_dqdp, t, k, nrint):
                 q = q_and_dqdp[0:self._n_states]
-                dqdp = q_and_dqdp[self._n_states:]#.reshape((self._n_states,
-                                                            #self._n_odeparams + self._n_ivs))
+                dqdp = q_and_dqdp[self._n_states:].reshape((self._n_states,
+                                                            self._n_odeparams + self._n_ivs))
                 dqdt = r(q, t, k, nrint)
 
+                # print('jacobian',jac(q))
+                # print('dqdp',dqdp)
+                # print('dfdp',dfdp(q,t,nrint))
 
-                jacobian = jac(q,t,nrint)
-                print('dqdp',dqdp.shape)
-                print('jacobian',jacobian.shape)
+                d_dqdp_dt = jac(q)*dqdp + dfdp(q,t,nrint) # CHANGED CODE HERE np.matmul(jac(q), dqdp) + dfdp(q,t,nrint)
+                return np.concatenate((dqdt, d_dqdp_dt.reshape(-1)))
 
-                d_dqdp_dt = np.matmul(jac(q,t,nrint), dqdp) + dfdp(q)
-                return np.array((dqdt, d_dqdp_dt))#.reshape(-1)))
-
-            y0 = np.zeros( (2*(n_odeparams+n_ivs)) + n_states )
-            y0[1] = 1.            #\frac{\partial  [X]}{\partial Xt0} at t==0, and same below for Y
+            y0 = np.zeros( (n_states*(n_odeparams+n_ivs)) + n_states ) # CHANGED CODE HERE 2*
+            y0[2] = 1.            #\frac{\partial  [X]}{\partial Xt0} at t==0, and same below for Y
             y0[0:n_states] = q0
             result = odeint(rhs, y0, times, (k,nr_int),rtol=1e-6,atol=1e-5)
             values = result[:, 0:self._n_states]
@@ -75,8 +75,6 @@ class LinearReservoirModel(object):
 
             # Solve ODE
             q = odeint(r,q0,times,args=(k,nr_int),rtol=1e-6,atol=1e-5)
-            q_flat = [max(item,0) for sublist in q for item in sublist]
+            q_flat = [item for sublist in q for item in sublist]
 
             return q_flat
-
-# ode_model = LinearReservoirModel()
