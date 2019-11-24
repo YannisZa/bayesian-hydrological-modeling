d886f866f99fd52951eef346a26170e003143fc3
Yannis <yzachos@gmail.com>
2019-11-24 23:46:43 +0000

    fixed bugs in LRSM

--- models/statistical/LinearReservoirStatisticalModel.py
+++ models/statistical/LinearReservoirStatisticalModel.py
@@ -4,6 +4,7 @@ import numpy as np
 import pymc3 as pm
 import theano
 from theano import *
+import theano.tensor as tt
 import math
 theano.config.exception_verbosity= 'high'
 theano.config.floatX = 'float64'
@@ -11,7 +12,7 @@ print('Running on PyMC3 v{}'.format(pm.__version__))
 
 n_states = 1
 n_odeparams = 1
-n_ivs = 0
+n_ivs = 1
 
 class LinearReservoirStatisticalModel(object):
 
@@ -22,16 +23,18 @@ class LinearReservoirStatisticalModel(object):
     def run(self,q,params):
 
         # Define the data matrix
-        Q = np.stack([q]).T
+        Q = np.vstack((q))
+
+        # print(Q.shape)
 
         # Define times
         times = np.arange(0,len(q))
 
-        # Instantiate LRM model
+        # Instantiate Linear Reservoir model
         lrm = LRM(n_states, n_odeparams, n_ivs, self._nr)
 
         # Instantiate Run object
-        r = Run(lrm, times, 1, 1)
+        r = Run(lrm, times, n_states, n_ivs, n_odeparams)
 
         # Now instantiate the theano custom ODE op
         my_ODEop = ODEop(r.state,r.numpy_vsp)
@@ -39,24 +42,40 @@ class LinearReservoirStatisticalModel(object):
         with pm.Model() as lrs_model:
 
             # Priors for unknown model parameters
-            k =  pm.Uniform('k', lower=0.01, upper=params.kmax)
+            k = pm.Uniform('k', lower=0.01, upper=params.kmax)
+            # k = pm.Lognormal('k', mu=params.kmax, sd=1)
 
-            q0 = pm.Lognormal('q0', mu=math.log(params.muq), sd=math.log(params.sdq))
-
-            sigma = pm.Lognormal('sigma', mu=math.log(params.musigma), sd=math.log(params.sdsigma), shape=1)
+            # Priors for initial conditions and noise level
+            q0 = pm.Lognormal('q0', mu=params.muq, sd=params.sdq)
+            sigma = pm.Lognormal('sigma', mu=params.musigma, sd=params.sdsigma, shape=1)
 
             # Forward model
             all_params = pm.math.stack([k,q0],axis=0)
             ode_sol = my_ODEop(all_params)
-            forward = ode_sol.reshape(Y.shape)
+            forward = ode_sol.reshape(Q.shape)
+
+            # Print ODE solution
+            # log_forward = pm.math.log(forward)
+            # log_forward_print = tt.printing.Print('log_forward')(log_forward.shape)
+            # tt.printing.Print('sigma')(sigma.shape)
+            # print(np.log(Q))
+            # print(Q)
 
             # Likelihood
             Q_obs = pm.Lognormal('Q_obs', mu=pm.math.log(forward), sd=sigma, observed=Q)
+            # Q_obs_print = tt.printing.Print('Q_obs')(Q_obs)
 
-            # Using Metropolis Hastings Sampling
-            step = pm.Metropolis()
+            # for RV in lrs_model.basic_RVs:
+            #     print(RV.name, RV.logp(lrs_model.test_point))
+            print(lrs_model.check_test_point())
+
+            # # Using Metropolis Hastings Sampling
+            # step = pm.Metropolis()
 
             # Draw the specified number of samples
-            discharge_trace = pm.sample(params.N_SAMPLES, step=step, tune=( max(params.N_SAMPLES - 500, int(params.N_SAMPLES/3)) ), init='adapt_diag')
+            # advi+adapt_diag
+            discharge_trace = pm.sample(draws=params.nsamples, chains=params.nchains, tune=params.ntune, init='jitter+adapt_diag')
 
-            return trace
+        print(discharge_trace['diverging'].sum())
+
+        return discharge_trace
