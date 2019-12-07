1a5774bbcf527227d892ad9c66f27fe31816cf36
Yannis <yzachos@gmail.com>
2019-12-07 16:40:32 +0000

    added scaling factor to model data simulation

--- simulators/nonlinear_reservoir_simulation.py
+++ simulators/nonlinear_reservoir_simulation.py
@@ -32,32 +32,38 @@ parser.add_argument("--simulate",dest='simulate', action='store_true',
                     help="sets flag for whether to simulate or use synthetic data for rainfall and evapotranspiration to true")
 parser.add_argument("--no-simulate",dest='simulate', action='store_false',
                     help="sets flag for whether to simulate or use synthetic data for rainfall and evapotranspiration to false")
-parser.add_argument("-i", "--input_filename",nargs='?',type=str,default = 'synthetic/rainfall_evapotranspiration_syn.csv',
-                    help="filename of input dataframe (must end with .csv)")
+parser.add_argument("-i", "--input_filename",nargs='?',type=str,default = 'raw/road_data.csv',
+                    help="filename of input dataframe (must end with .csv) (default: %(default)s)")
 parser.add_argument("-o", "--output_filename",nargs='?',type=str,default = 'simulations/nonlinear_reservoir_simulation.csv',
-                    help="filename of output dataframe (must end with .csv)")
-parser.add_argument("-k", "--k",nargs='?',type=float,default = 4.0,
-                    help="constant reaction factor or response factor with unit T (must be positive)")
-parser.add_argument("-m", "--m",nargs='?',type=float,default = 0.3,
+                    help="filename of output dataframe (must end with .csv) (default: %(default)s)")
+parser.add_argument("-k", "--k",nargs='?',type=float,default = 1.1,
+                    help="constant reaction factor or response factor with unit T (must be positive) (default: %(default)s)")
+parser.add_argument("-m", "--m",nargs='?',type=float,default = 0.7,
                     help="store exponent (must be positive)")
 parser.add_argument("-q", "--q0",nargs='?',type=float,default = 0.01,
                     help="value of discharge (q) at time 0 (must be positive)")
-parser.add_argument("-s", "--sigma",nargs='?',type=float,default = 0.05,
-                    help="Standard deviation of white Gaussian noise N(0,s^2) to be added to discharge ")
-parser.add_argument("-a", "--catchment_area",nargs='?',type=float,default = 3600.0,
-                    help="Area of catchment (in m^2) to be multiplied with discharge")
+parser.add_argument("-s", "--sigma",nargs='?',type=float,default = 0.5,
+                    help="Standard deviation of white Gaussian noise N(0,s^2) to be added to discharge (default: %(default)s)")
+parser.add_argument("-a", "--catchment_area",nargs='?',type=float,default = 15966.0,
+                    help="Area of catchment (in m^2) to be multiplied with discharge (default: %(default)s)")
 parser.add_argument("-t", "--tdelta",nargs='?',type=int,default = 1,
-                    help="timestep to normalise time by to make the time units seconds")
+                    help="timestep to normalise time by to make the time units seconds (default: %(default)s)")
 parser.add_argument("-r", "--randomseed",nargs='?',type=int,default = 22,
                     help="fixed random seed for generating noise")
 args = parser.parse_args()
 params = vars(args)
+
+minvalue = 0.0001
 
 # Get current working directory and project root directory
 cwd = os.getcwd()
 rd = os.path.join(cwd.split('fibe2-mini-project/', 1)[0])
 if not rd.endswith('fibe2-mini-project'):
     rd = os.path.join(cwd.split('fibe2-mini-project/', 1)[0],'fibe2-mini-project')
+
+# Set conversion factor
+args.fatconv = 1. / 1000.0 / args.tdelta * args.catchment_area
+print('fatconv',args.fatconv)
 
 # Export model parameters
 with open(os.path.join(rd,'data','output',args.output_filename.replace('.csv','_true_parameters.json')), 'w') as f:
@@ -69,12 +75,12 @@ print(json.dumps(params,indent=2))
 
 ''' Generate or read discharge data '''
 
-# Set conversion factor
-fatconv = 1. / 1000.0 / args.tdelta * args.catchment_area
-
 # Compute derived parameters
 a = args.m*(1./args.k)**(1./args.m)
 b = (args.m-1)/args.m
+
+print('a',a)
+print('b',b)
 
 # Raise ValueError if m is negative
 if args.m <= 0:
@@ -95,7 +101,7 @@ if args.simulate:
     et[zero_et_days] = 0
 
     # Compute
-    nr = [rft - ett for rft, ett in zip(rf, et)]
+    nr = [max(rft - ett,minvalue) for rft, ett in zip(rf, et)]
 
 else:
     print('Reading input data')
@@ -103,7 +109,7 @@ else:
     rf = df['rainfall'].values.tolist()
     et = df['evapotranspiration'].values.tolist()
     # Compute
-    nr = [max(rft - ett,0.0001) for rft, ett in zip(rf, et)]
+    nr = [max(rft - ett,minvalue) for rft, ett in zip(rf, et)]
 
 # Number of timesteps
 n = len(nr)
@@ -115,22 +121,26 @@ time = range(0,n)
 nr_int = interp1d(time, nr,fill_value="extrapolate",kind='slinear')
 
 # Define model
-def nonlinear_reservoir_model(q,time,nrint):
-    return ( a * ( nrint(time) - q) * (q**b) )
+def nonlinear_reservoir_model(q,t,nrint):
+    return ( a * ( max(minvalue,nrint(t)) - q) * (q**b) )
 
 
 # Solve ODE
 q = odeint(nonlinear_reservoir_model,args.q0,time,args=(nr_int,))
 # Flatten q
-q_flat = [item * fatconv for sublist in q for item in sublist]
+q_flat = [item * args.fatconv for sublist in q for item in sublist]
+q_flat = [q_flat[i] for i in range(0,n)]
 
 # Fix random seed
 np.random.seed(args.randomseed)
 # Add Gaussian noise to discharge
 Q_sim = np.asarray(q_flat).reshape(n,1) + np.random.randn(n,1)*args.sigma
+Q_sim = [max(0.0,qsim[0]) for qsim in Q_sim]
 
 ''' Export data to file '''
-df = pd.DataFrame(list(zip(time,rf,et,nr,Q_sim.reshape(n,))), columns =['time', 'rainfall','evapotranspiration','net_rainfall', 'discharge'])
+df = pd.DataFrame(list(zip(time,rf,et,nr,Q_sim)), columns =['time', 'rainfall','evapotranspiration','net_rainfall', 'discharge'])
+print(df.head(10))
+
 df.to_csv(os.path.join(rd,'data','output',args.output_filename),index=False)
 
 print('Done!...')
