447ab8d2cc0c5beaf005ec188c8e65a52a6cc83d
Yannis Zachos <yzachos@gmail.com>
2020-02-06 13:30:46 +0000

    Delete chess-evapotranspiration-data.py

--- data-access/chess-evapotranspiration-data.py
+++
@@ -1,7 +0,0 @@
-
-
-# List of all datasets
-# https://catalogue.ceh.ac.uk/datastore/eidchub/b745e7b1-626c-4ccc-ac27-56582e77b900/
-
-
-filename = '/Users/Yannis/code/fibe2-mini-project/Historic_daily_PET_McGuinness-Bordne_2015.nc'
